f02a513e71b25859518b600eb9f0981f54473914
Student <Student@10.111.26.175>
2022-01-27 15:20:17 -0500

    new classes created

---
+++ src/main/java/edu/fiu/surya/roomba/InfraredSensor.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package edu.fiu.surya.roomba;
+
+/**
+ * @author Student
+ *
+ */
+public class InfraredSensor {
+	private String dimension;
+	private String range;
+	private String manufacturer;
+	private int detectionangle;
+	
+	public void detectObstacle()
+	{
+		//
+	}
+	
+	public void mapFloor()
+	{
+		//
+	}
+
+}	
---
+++ src/main/java/edu/fiu/surya/roomba/vacuum.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.fiu.surya.roomba;
+
+/**
+ * @author Student
+ *
+ */
+public class vacuum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Test");
+	}
+
+}
